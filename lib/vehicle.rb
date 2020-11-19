fafbc459e5ff10d218e5216cc80cba795ddf6d5a
John Million <jpmillion@comcast.net>
2020-11-19 14:49:21 +0000

    Automatically backed up by Learn

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -2,6 +2,9 @@ class Vehicle
   
   attr_accessor :wheel_size, :wheel_number
   
-  
+  def initialize(wheel_size, wheel_number)
+    @wheel_number = wheel_number
+    @wheel_size = wheel_size
+  end
   
 end
